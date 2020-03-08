457622dc053b84bf2879979da6b26dd253121c45
Diego Coronado <diegocoronado8@hotmail.com>
2020-03-07 23:56:05 -0500

    code refactor and layout for items

--- app/src/main/java/com/diego_cor/MainActivity.kt
+++ app/src/main/java/com/diego_cor/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.diego_cor
 
+import android.content.Context
 import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import kotlinx.android.synthetic.main.activity_main.*
 import java.io.BufferedReader
 import java.io.IOException
 import java.io.InputStreamReader
@@ -12,23 +16,57 @@ import java.lang.StringBuilder
 import java.net.HttpURLConnection
 import java.net.MalformedURLException
 import java.net.URL
+import kotlin.properties.Delegates
+
+class FeedEntry {
+    var name: String = ""
+    var artist: String = ""
+    var releaseDate: String = ""
+    var summary: String = ""
+    var imageURL: String = ""
+
+    override fun toString(): String {
+        return """
+            name = $name
+            artist = $artist
+            releaseDate = $releaseDate
+            imageURL = $imageURL
+        """.trimIndent()
+    }
+}
 
 class MainActivity : AppCompatActivity() {
-    private val TAG = "MainActivity";
+    private val TAG = "MainActivity"
+
+    private val downloadData by lazy { DownloadData(this, xmlListView) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         Log.d(TAG, "oncreate called")
 
-        val downloadData = DownloadData()
+        //val downloadData = DownloadData(this, xmlListView)
         downloadData.execute("http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topfreeapplications/limit=10/xml")
         Log.d(TAG, "oncreate: done")
     }
 
+    override fun onDestroy() {
+        super.onDestroy()
+
+        downloadData.cancel(true)
+    }
+
     companion object {
-        private class DownloadData : AsyncTask<String, Void, String>() {
+        private class DownloadData(context: Context, listView: ListView) : AsyncTask<String, Void, String>() {
             private val TAG = "DownloadData"
+
+            var propContext : Context by Delegates.notNull()
+            var propListView: ListView by Delegates.notNull()
+
+            init {
+                propContext = context
+                propListView = listView
+            }
 
             override fun doInBackground(vararg params: String?): String {
                 Log.d(TAG, "doinbackground: starts with ${params[0]}")
@@ -39,51 +77,18 @@ class MainActivity : AppCompatActivity() {
                 return rssFeed
             }
 
-            override fun onPostExecute(result: String?) {
+            override fun onPostExecute(result: String) {
                 super.onPostExecute(result)
-                Log.d(TAG, "onpostexecute: parameter is $result")
+//                Log.d(TAG, "onpostexecute: parameter is $result")
+                val parseAplications = ParseApplications()
+                parseAplications.parse(result)
+
+                val arrayAdapter = ArrayAdapter<FeedEntry>(propContext, R.layout.list_item, parseAplications.applications)
+                propListView.adapter = arrayAdapter
             }
 
             private fun downloadXML(urlPath: String?): String {
-                val xmlResult = StringBuilder()
-
-                try {
-                    val url = URL(urlPath)
-                    val connection: HttpURLConnection = url.openConnection() as HttpURLConnection
-                    val response = connection.responseCode
-                    Log.d(TAG, "downloadxml: the response code was $response")
-
-
-//                    val reader = BufferedReader(InputStreamReader(connection.inputStream))
-//                    val inputBuffer = CharArray(500)
-//                    var charsRead = 0
-//                    while (charsRead >= 0) {
-//                        charsRead = reader.read(inputBuffer)
-//                        if (charsRead > 0) {
-//                            xmlResult.append((String(inputBuffer, 0, charsRead)))
-//                        }
-//                    }
-//                    reader.close()
-
-                    connection.inputStream.buffered().reader().use { reader ->
-                        xmlResult.append(reader.readText())
-                    }
-
-                    Log.d(TAG, "Received ${xmlResult.length} bytes")
-                    return xmlResult.toString()
-                } catch (e: Exception) {
-                    val errorMessage: String = when (e) {
-                        is MalformedURLException -> "downloadXml:Invalid url ${e.message}"
-                        is IOException -> "downloadxml: IO exception reading data: ${e.message}"
-                        is SecurityException -> {
-                            e.printStackTrace()
-                            "doanloadxml: Security exception needs permission? ${e.message}"
-                        }
-                        else -> "downloadxml: Unknown error: ${e.message}"
-                    }
-                }
-
-                return ""
+                return URL(urlPath).readText()
             }
         }
     }
